c6cae96b0117c509714a44be7d4d852474919a4b
Salah Eddin Kanaya <saladdin@Salahs-MBP.lan>
2019-04-27 17:19:43 +0200

    Initial Commit

---
+++ Upplaod to githup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Upplaod to githup
+//
+//  Created by Salah Eddin Kanaya on 2019-04-27.
+//  Copyright © 2019 Kungshögaskolan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
